b1009059a01c435c0d0f20496212127dfe8e24b3
Kim Alexander <128368574+brdchy@users.noreply.github.com>
2023-11-06 16:29:33 +1000

    Begula (#4)
    * fix resolution
    
    * animation
    
    * added shooting
    
    * meat from enemy
    
    * weapon
    
    ---------
    
    Co-authored-by: Cleamaster322 <82811442+Cleamaster322@users.noreply.github.com>
    Co-authored-by: bennnnnja <nikitayurov95@gmail.com>

--- Mutational-Madness/Assets/Scripts/Gun.cs
+++ Mutational-Madness/Assets/Scripts/Gun.cs
@@ -7,7 +7,8 @@ public class Gun : MonoBehaviour
     public float offset;
     public GameObject bullet;
     public Transform shotPoint;
-    
+    public Player player;
+
 
 
     private float timeBtwShots;
@@ -17,22 +18,25 @@ public class Gun : MonoBehaviour
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-        
 
 
-        if (timeBtwShots <= 0)
+        if (player.weapon == 2)
         {
-            if (Input.GetMouseButton(0)) 
+            if (timeBtwShots <= 0)
             {
-                Instantiate(bullet, shotPoint.position, transform.rotation); 
-                timeBtwShots = startTimeBtwShots; 
+                if (Input.GetMouseButton(0))
+                {
+                    Instantiate(bullet, shotPoint.position, transform.rotation);
+                    timeBtwShots = startTimeBtwShots;
+                }
+            }
+
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
             }
         }
 
-        else
-        {
-            timeBtwShots -= Time.deltaTime; 
         }
-        
     }
-}
+
--- Mutational-Madness/Assets/Scripts/player.cs
+++ Mutational-Madness/Assets/Scripts/player.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
     [Header("Player Animation Settings")] 
-    public Animator animator; 
+
+    public Animator animator;
+    public int weapon;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        weapon = 1;
     }
 
     void Update()
@@ -31,9 +35,24 @@ public class PlayerMovement : MonoBehaviour
 
         animator = GetComponent<Animator>();
 
-        animator.SetFloat("moveX", (moveX));
-        animator.SetFloat("moveY", (moveY));
 
-        
+        animator.SetFloat("moveX", moveX);
+        animator.SetFloat("moveY", moveY);
+        animator.SetInteger("weapon", weapon);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            weapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            weapon = 2;
+        }
+
+
     }
 }
