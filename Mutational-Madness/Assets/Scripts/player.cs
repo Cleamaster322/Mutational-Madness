9f355a6b2e861988878ad8f7bf39c9e03b7a9a7d
bennnnnja <nikitayurov95@gmail.com>
2023-11-06 19:08:02 +1000

    added sword animation and attack

--- Mutational-Madness/Assets/Scripts/player.cs
+++ Mutational-Madness/Assets/Scripts/player.cs
@@ -10,17 +10,25 @@ public class Player : MonoBehaviour
     [Header("Player Animation Settings")]
     public Animator animator;
     public int weapon;
+    public float attackRange;
+    public LayerMask Solid;
+    public float lastDirection;
+    public int damage;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        weapon = 1; 
+        weapon = 1;
     }
 
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+        if (Mathf.Abs(moveX) > 0.1f)
+        {
+            lastDirection = Mathf.Sign(moveX);
+        }
 
         Vector2 movement = new Vector2(moveX, moveY);
 
@@ -32,7 +40,7 @@ public class Player : MonoBehaviour
         animator.SetFloat("moveY", moveY);
         animator.SetInteger("weapon", weapon);
 
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("MainMenu");
         }
@@ -44,6 +52,22 @@ public class Player : MonoBehaviour
         {
             weapon = 2;
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
 
+    }
+    void Attack()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, attackRange, Solid);
+        foreach (var hit in hits)
+        {
+            Enemy enemy = hit.GetComponent<Enemy>();
+            if (enemy != null && Mathf.Sign(enemy.transform.position.x - transform.position.x) == lastDirection)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
     }
 }
