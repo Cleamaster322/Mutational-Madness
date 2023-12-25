a202e98eae78b15266ec3805a7fbb7f4e7c52f3b
Kim Alexander <kim.aeduard@students.dvfu.ru>
2023-12-25 23:45:20 +1000

    last stable

--- Mutational-Madness/Assets/Scripts/RoomTrigger.cs
+++ Mutational-Madness/Assets/Scripts/RoomTrigger.cs
@@ -7,7 +7,12 @@ public class ActivationZone : MonoBehaviour
     public GameObject[] enemies;
     public float fallSpeed = 1f;
     private int flag;
-
+    public int room;
+    private Player player;
+    void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
     private void Start()
     {
         foreach (var enemy in enemies)
@@ -25,7 +30,11 @@ public class ActivationZone : MonoBehaviour
             flag = 1;
             foreach (var enemy in enemies)
             {              
-                    enemy.SetActive(true);        
+                enemy.SetActive(true);        
+            }
+            if (room == 1)
+            {
+                player.weapon = 1;
             }
         }
     }
