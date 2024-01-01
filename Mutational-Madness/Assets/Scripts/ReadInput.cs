617bd5eb39a18e722c7397d895d9475dddfa2364
Kim Alexander <kim.aeduard@students.dvfu.ru>
2024-01-02 05:58:05 +1000

    now it's actually working

--- Mutational-Madness/Assets/Scripts/ReadInput.cs
+++ Mutational-Madness/Assets/Scripts/ReadInput.cs
@@ -9,32 +9,24 @@ public class ReadInput : MonoBehaviour
     public GameObject gg;
     public GameObject Trent;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
     public void ReadStringInput(string s)
     {
         input = s;
         if (input == "PanSan!")
         {
+            Trent.SetActive(false);
             gg.SetActive(false);
             PanSan.SetActive(true);
         }
-        if (input == "Xmas!")
-        {
-            gg.SetActive(false);
-            Trent.SetActive(true);
-        }
+        
 
         if (input == "Wham!")
         {
+            PanSan.SetActive(false);
+            gg.SetActive(false);
+            Trent.SetActive(true);
             System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=E8gmARGvPlI");
         }
     }
