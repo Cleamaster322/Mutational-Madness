64a88807396a550b6da1fc7d6044bd630bed1e8f
Kim Alexander <128368574+brdchy@users.noreply.github.com>
2024-01-09 12:57:42 +1000

    show me

--- Mutational-Madness/Assets/Scripts/MenuMusicManager.cs
+++ Mutational-Madness/Assets/Scripts/MenuMusicManager.cs
@@ -5,7 +5,7 @@ public class MusicManager : MonoBehaviour
 {
     public static MusicManager instance = null;
     private AudioSource audioSource;
-
+    private AudioSource gameAudioSource;
     void Awake()
     {
        
@@ -17,23 +17,32 @@ public class MusicManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         audioSource = gameObject.AddComponent<AudioSource>();
+        gameAudioSource = gameObject.AddComponent<AudioSource>();
     }
 
     void Start()
     {
-        string path = "Assets/gamesound/menu.mp3";
+        string path = "Assets/gamesound/ingame1slow.mp3";
         AudioClip clip = (AudioClip)AssetDatabase.LoadAssetAtPath(path, typeof(AudioClip));
         audioSource.clip = clip;
         audioSource.volume = 1f;
         audioSource.loop = true;
         audioSource.Play();
+        string pathGame = "Assets/gamesound/menu.mp3";
+        AudioClip clipGame = (AudioClip)AssetDatabase.LoadAssetAtPath(pathGame, typeof(AudioClip));
+        gameAudioSource.clip = clipGame;
+        gameAudioSource.volume = 0.5f;
+        gameAudioSource.loop = true;
+        
     }
     public void StopMusic()
     {
         audioSource.Stop();
+        gameAudioSource.Play();
     }
     public void PlayMusic()
     {
         audioSource.Play();
+        gameAudioSource.Stop();
     }
 }
